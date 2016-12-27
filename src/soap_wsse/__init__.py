f4d79a9cbd7dbf494a021077061ec4cfe18c5f66
ynalcakan <yagiznalcakan@gmail.com>
2016-12-27 14:12:38 +0300

    dosya yapısı güncelleme

--- src/soap_wsse/__init__.py
+++ src/soap_wsse/__init__.py
@@ -1,2 +1,2 @@
-from soap_wsse.signing import sign_envelope, verify_envelope  # NOQA
-from soap_wsse.signing import CertificationError  # NOQA
+from src.soap_wsse.signing import sign_envelope, verify_envelope  # NOQA
+from src.soap_wsse.signing import CertificationError  # NOQA
